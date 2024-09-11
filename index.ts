0a3b08be1e26912cea91472b4dba7ca2b8c59372
Mathesh M <157198679+Mathesh-299@users.noreply.github.com>
2024-09-11 16:27:46 +0530

    all

---
+++ index.ts
@@ -0,0 +1,11 @@
+function myfunction(data1:number,data2:number){
+    if(data2){
+        console.log(data1+data2);
+    }
+    else{
+        console.log("Not available");
+    }
+}
+// myfunction(1);
+// myfunction(1,true);
+myfunction(1,2);
