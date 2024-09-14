e0b6ef01f6f67f3d28f3a8ba9b86acdab576e493
Mathesh M <157198679+Mathesh-299@users.noreply.github.com>
2024-09-14 12:31:55 +0530

    async

--- callback.js
+++ callback.js
@@ -1,11 +1,11 @@
-// function f1(data){
-//     console.log(`hello : f1: ${data}`);
-//     f2();
-// }
-// function f2(){
-//     console.log("hello : f2");
-// }
-// setTimeout(f1,3000,"hello");
+function f1(data){
+    console.log(`hello : f1: ${data}`);
+    f2();
+}
+function f2(){
+    console.log("hello : f2");
+}
+setTimeout(f1,3000,"hello");
 
 function f1(data){
     console.log(`hello : f1 : ${data}`);
@@ -46,3 +46,37 @@ function balancechecker(){
 }
 
 balancechecker()
+
+
+
+async function api(){
+    const response=await fetch('https://66e526e55cc7f9b6273c6bb6.mockapi.io/Register')
+    const data1=await response.json()
+    //console.log(data1)
+    // return data1;
+    
+    try {
+        const response1=await fetch('https://66e526e55cc7f9b6273c6bb6.mockapi.io/Register')
+        const data12=await response1.json()
+        //console.log(data12)
+        return data12
+
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+// api()
+async function apidata1() {
+    const apidata=await api()
+    //console.log(apidata)
+    const modedata=[{
+        "name":"hello"
+    }]
+    const stru=modedata.map((data)=>{console.log(data)})
+    const structure=apidata.map((data)=>{console.log(data)})
+    
+}
+
+apidata1()
